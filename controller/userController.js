58b2c0dcd33768da424774fe2daee761c327424f
DebasishKisan1 <debasish.kisan@gmail.com>
2023-12-15 21:35:55 +0530

    login backend

--- controller/userController.js
+++ controller/userController.js
@@ -2,9 +2,15 @@ const path = require('path')
 const User = require('../model/userModel')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
+const jwt = require('jsonwebtoken')
+
 
 exports.getHome = (req, res, next) => {
     res.sendFile(path.join(__dirname, '../views/', 'signup.html'))
+}
+
+exports.getLogin = (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../views/', 'login.html'))
 }
 
 
@@ -31,6 +37,33 @@ exports.postSignup = async (req, res, next) => {
             return res.status(201).json({ message: 'User Created successfully' })
         } else {
             return res.status(200).json({ message: 'User Already Exist Please Login' })
+        }
+    } catch (err) {
+        return res.status(500).json({ message: 'Some Error Occured' })
+    }
+}
+
+
+
+exports.postLogin = async (req, res, next) => {
+    const { email, password } = req.body
+    try {
+        const user = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+        if (user) {
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (result) {
+                    var token = jwt.sign(user.id, process.env.pvtkey)
+                    return res.status(200).json({ message: "User Login Successfully", userid: token })
+                } else {
+                    return res.status(200).json({ message: "Incorrect Credentials" })
+                }
+            })
+        } else {
+            return res.status(200).json({ message: 'User Not Found' })
         }
     } catch (err) {
         return res.status(500).json({ message: 'Some Error Occured' })
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const userController = require('../controller/userController')
 
+router.get('/login',userController.getLogin)
 router.get('/',userController.getHome)
 
 router.post('/user/signup',userController.postSignup)
+router.post('/user/login',userController.postLogin)
 
 module.exports = router;
